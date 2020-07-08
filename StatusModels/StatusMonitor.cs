816ec4fd7130dc53379149e1742734c292361c5a
JamesRobertLundberg <JamesRobertLundberg@hotmail.com>
2020-07-08 16:43:08 -0700

    Fixed bug with Index Page

--- StatusModels/StatusMonitor.cs
+++ StatusModels/StatusMonitor.cs
@@ -7,7 +7,8 @@ namespace StatusModels
     public class StatusMonitorData
     {
         public string Job { get; set; }
-        public string JobPrefix { get; set; }
+        public string JobName { get; set; }
+        public string TimeStamp { get; set; }
         public string JobDirectory { get; set; }
         public string IniFileName { get; set; }
         public string XmlFileName { get; set; }
@@ -16,7 +17,7 @@ namespace StatusModels
         public string RepositoryDir { get; set; }
         public string FinishedDir { get; set; }
         public string ErrorDir { get; set; }
-        public string ModelerRootDir { get; set; }
+        public string ModelersDir { get; set; }
         public int CPUCores { get; set; }
         public string Modeler { get; set; }
         public int MaxTimeLimit { get; set; }
@@ -26,5 +27,10 @@ namespace StatusModels
         public int NumFilesConsumed { get; set; }
         public int NumFilesProduced { get; set; }
         public int NumFilesToTransfer { get; set; }
+        public string Transfered1 { get; set; }
+        public string Transfered2 { get; set; }
+        public string Transfered3 { get; set; }
+        public string Transfered4 { get; set; }
+        public string Transfered5 { get; set; }
     }
 }
