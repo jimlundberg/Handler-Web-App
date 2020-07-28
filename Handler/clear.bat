d546effd94caa15515badf9cd98b23967ea4a51c
JamesRobertLundberg <JamesRobertLundberg@hotmail.com>
2020-07-28 02:22:18 -0700

    Added TCP/IP handling

---
+++ Handler/clear.bat
@@ -0,0 +1,18 @@
+:RemoveDirectories
+
+rmdir /s /q "Input Buffer"
+md "Input Buffer"
+
+rmdir /s /q ProcessingBuffer
+md ProcessingBuffer
+
+rmdir /s /q Archive
+md Archive
+
+rmdir /s /q "Error Buffer"
+md "Error Buffer"
+
+rmdir /s /q "Output Buffer"
+md "Output Buffer"
+
+cls
--- Status.Services/StatusRepository.cs
+++ Status.Services/StatusRepository.cs
@@ -505,93 +505,6 @@ namespace Status.Services
     }
 
     /// <summary>
-    /// Class to monitor and report status the TCP/IP connection to the Monitor application that is executing 
-    /// </summary>
-    public class TcpIpConnection
-    {
-        public static System.Timers.Timer aTimer;
-        static public int PortNumber;
-
-        /// <summary>
-        /// Connect to TCP/IP Port
-        /// </summary>
-        /// <param name="server"></param>
-        /// <param name="port"></param>
-        /// <param name="message"></param>
-        static void Connect(String server, Int32 port, String message)
-        {
-            try
-            {
-                // Set current port number
-                PortNumber = port;
-
-                // Create a TcpClient.
-                // Note, for this client to work you need to have a TcpServer
-                // connected to the same address as specified by the server, port combination.
-                TcpClient client = new TcpClient(server, PortNumber);
-
-                // Translate the passed message into ASCII and store it as a Byte array.
-                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
-
-                // Get a client stream for reading and writing.
-                // Stream stream = client.GetStream();
-                NetworkStream stream = client.GetStream();
-
-                // Send the message to the connected TcpServer.
-                stream.Write(data, 0, data.Length);
-
-                // Receive the TcpServer.response.
-                Console.WriteLine("Sent: {0}", message);
-
-                // Buffer to store the response bytes.
-                data = new Byte[256];
-
-                // String to store the response ASCII representation.
-                String responseData = String.Empty;
-
-                // Read the first batch of the TcpServer response bytes.
-                Int32 bytes = stream.Read(data, 0, data.Length);
-                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
-                Console.WriteLine("Received: {0}", responseData);
-
-                // Close everything.
-                stream.Close();
-                client.Close();
-            }
-            catch (ArgumentNullException e)
-            {
-                Console.WriteLine("ArgumentNullException: {0}", e);
-            }
-            catch (SocketException e)
-            {
-                Console.WriteLine("SocketException: {0}", e);
-            }
-
-            Console.WriteLine("\n Press Enter to exit...");
-            Console.Read();
-        }
-
-        public static void SetTimer()
-        {
-            // Create a timer with a five second interval.
-            aTimer = new System.Timers.Timer(60000);
-
-            // Hook up the Elapsed event for the timer. 
-            aTimer.Elapsed += OnTimedEvent;
-            aTimer.AutoReset = true;
-            aTimer.Enabled = true;
-        }
-
-        public static void OnTimedEvent(Object source, ElapsedEventArgs e)
-        {
-            // Check modeler status
-            Console.WriteLine("Modeler status sent at {0:HH:mm:ss.fff} on port {1}", e.SignalTime, PortNumber);
-            Connect("127.0.0.1", PortNumber, "status");
-            Console.ReadLine();
-        }
-    }
-
-    /// <summary>
     /// Status Entry class
     /// </summary>
     public class StatusEntry
@@ -792,6 +705,174 @@ namespace Status.Services
     }
 
     /// <summary>
+    /// TCP/IP Thread Class
+    /// </summary>
+    public class TcpIpThread
+    {
+        // State information used in the task.
+        private IniFileData IniData;
+        private StatusMonitorData MonitorData;
+        private List<StatusWrapper.StatusData> StatusData;
+
+        // The constructor obtains the state information.
+        public TcpIpThread(IniFileData iniData, StatusMonitorData monitorData, List<StatusWrapper.StatusData> statusData)
+        {
+            IniData = iniData;
+            MonitorData = monitorData;
+            StatusData = statusData;
+        }
+
+        // The thread procedure performs the task
+        public void ThreadProc()
+        {
+            TcpIpMonitor(MonitorData.JobPortNumber);
+        }
+
+        public void StatusEntry(List<StatusWrapper.StatusData> statusList, String job, JobStatus status, JobType timeSlot)
+        {
+            StatusWrapper.StatusData entry = new StatusWrapper.StatusData();
+            entry.Job = job;
+            entry.JobStatus = status;
+            switch (timeSlot)
+            {
+                case JobType.TIME_START:
+                    entry.TimeStarted = DateTime.Now;
+                    break;
+
+                case JobType.TIME_RECEIVED:
+                    entry.TimeReceived = DateTime.Now;
+                    break;
+
+                case JobType.TIME_COMPLETE:
+                    entry.TimeCompleted = DateTime.Now;
+                    break;
+            }
+
+            statusList.Add(entry);
+            Console.WriteLine("Status: Job:{0} Job Status:{1} Time Type:{2}", job, status, timeSlot.ToString());
+        }
+
+        public void TcpIpMonitor(int TcpIpPortNumber)
+        {
+            TcpIpConnection tcpIpConnection = new TcpIpConnection();
+            TcpIpConnection.PortNumber = MonitorData.JobPortNumber;
+            TcpIpConnection.SetTimer();
+            tcpIpConnection.Connect("127.0.0.1", TcpIpPortNumber, "status");
+
+            String response;
+            do
+            {
+                response = Console.ReadLine();
+                Console.WriteLine("Modeler TCP/IP response at {0:HH:mm:ss.fff} is {1}", DateTime.Now, response);
+
+                // Send status for response received
+                switch (response)
+                {
+                    case "Step 1 in process.":
+                        StatusEntry(StatusData, MonitorData.Job, JobStatus.MONITORING_TCPIP, JobType.TIME_START);
+                        break;
+
+                    case "Complete":
+                        StatusEntry(StatusData, MonitorData.Job, JobStatus.MONITORING_TCPIP, JobType.TIME_START);
+                        break;
+                }
+
+                Thread.Sleep(IniData.ScanTime);
+            }
+            while (response != "Complete");
+
+            TcpIpConnection.aTimer.Stop();
+            TcpIpConnection.aTimer.Dispose();
+        }
+
+        /// <summary>
+        /// Class to monitor and report status the TCP/IP connection to the Monitor application that is executing 
+        /// </summary>
+        public class TcpIpConnection
+        {
+            public static System.Timers.Timer aTimer;
+            static public int PortNumber;
+
+            /// <summary>
+            /// Connect to TCP/IP Port
+            /// </summary>
+            /// <param name="server"></param>
+            /// <param name="port"></param>
+            /// <param name="message"></param>
+            public void Connect(String server, Int32 port, String message)
+            {
+                try
+                {
+                    // Set current port number
+                    PortNumber = port;
+
+                    // Create a TcpClient.
+                    // Note, for this client to work you need to have a TcpServer
+                    // connected to the same address as specified by the server, port combination.
+                    TcpClient client = new TcpClient(server, PortNumber);
+
+                    // Translate the passed message into ASCII and store it as a Byte array.
+                    Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+                    // Get a client stream for reading and writing.
+                    // Stream stream = client.GetStream();
+                    NetworkStream stream = client.GetStream();
+
+                    // Send the message to the connected TcpServer.
+                    stream.Write(data, 0, data.Length);
+
+                    // Receive the TcpServer.response.
+                    Console.WriteLine("Sent: {0}", message);
+
+                    // Buffer to store the response bytes.
+                    data = new Byte[256];
+
+                    // String to store the response ASCII representation.
+                    String responseData = String.Empty;
+
+                    // Read the first batch of the TcpServer response bytes.
+                    Int32 bytes = stream.Read(data, 0, data.Length);
+                    responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                    Console.WriteLine("Received: {0}", responseData);
+
+                    // Close everything.
+                    stream.Close();
+                    client.Close();
+                }
+                catch (ArgumentNullException e)
+                {
+                    Console.WriteLine("ArgumentNullException: {0}", e);
+                }
+                catch (SocketException e)
+                {
+                    Console.WriteLine("SocketException: {0}", e);
+                }
+
+                Console.WriteLine("\n Press Enter to exit...");
+                Console.Read();
+            }
+
+            public static void SetTimer()
+            {
+                // Create a timer with a five second interval.
+                aTimer = new System.Timers.Timer(60000);
+
+                // Hook up the Elapsed event for the timer. 
+                aTimer.Elapsed += OnTimedEvent;
+                aTimer.AutoReset = true;
+                aTimer.Enabled = true;
+            }
+
+            public static void OnTimedEvent(Object source, ElapsedEventArgs e)
+            {
+                // Check modeler status
+                Console.WriteLine("Reading Modeler at {0:HH:mm:ss.fff} on port {1}", e.SignalTime, PortNumber);
+                Console.ReadLine();
+            }
+        }
+    }
+
+    /// <summary>
     /// Class to run a Job as a thread
     /// </summary>
     public class JobRunThread
@@ -947,33 +1028,9 @@ namespace Status.Services
             Console.WriteLine("***** Started Job {0} with Modeler {1} on port {2} with {3} CPU's",
                 monitorData.Job, monitorData.Modeler, monitorData.JobPortNumber, iniData.CPUCores);
 
-            //do
-            //{
-            //    // Timed listen for Modeler TCP/IP response
-            //    TcpIpConnection.SetTimer();
-            //    String response;
-            //    do
-            //    {
-            //        response = Console.ReadLine();
-            //        Console.WriteLine("Scan TCP/IP at {0:HH:mm:ss.fff}", DateTime.Now);
-            //        Console.WriteLine(response);
-
-            //        // Not sure what the messages are yet
-            //        switch (response)
-            //        {
-            //            case "Complete":
-            //                break;
-            //        }
-
-            //        Thread.Sleep(5000);
-            //    }
-            //    while (response != "Complete");
-
-            //    TcpIpConnection.aTimer.Stop();
-            //    TcpIpConnection.aTimer.Dispose();
-            //    Thread.Sleep(30000);
-            //}
-            //while (true);
+            // Start TCP/IP monitor thread
+            TcpIpThread tcpIpThread = new TcpIpThread(iniData, monitorData, statusData);
+            tcpIpThread.TcpIpMonitor(monitorData.JobPortNumber);
 
             // Add entry to status list
             StatusDataEntry(statusData, job, JobStatus.MONITORING_PROCESSING, JobType.TIME_START, iniData.LogFile);
