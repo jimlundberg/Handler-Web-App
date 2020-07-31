41884407cc0815188330bba4921698c05d1131bd
JamesRobertLundberg <JamesRobertLundberg@hotmail.com>
2020-07-30 22:05:53 -0700

    more cleanup

--- Handler/Pages/Index.cshtml.cs
+++ Handler/Pages/Index.cshtml.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using Status.Services;
 using StatusModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Handler.Pages
 {
