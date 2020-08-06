76e993f415498ff169e83c16bb33cceec863fecc
JamesRobertLundberg <JamesRobertLundberg@hotmail.com>
2020-08-06 00:15:32 -0700

    Added AddQuotesIfRequired()

--- Status.Services/StaticData.cs
+++ Status.Services/StaticData.cs
@@ -16,5 +16,12 @@
         {
             NumberOfJobsExecuting--;
         }
+        public static string AddQuotesIfRequired(string path)
+        {
+            return !string.IsNullOrWhiteSpace(path) ?
+                path.Contains(" ") && (!path.StartsWith("\"") && !path.EndsWith("\"")) ?
+                    "\"" + path + "\"" : path :
+                    string.Empty;
+        }
     }
 }
