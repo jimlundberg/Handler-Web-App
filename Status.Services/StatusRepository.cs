3a4ff61f169c84d3a8cc0fe22e6a89f79e6d9394
JamesRobertLundberg <JamesRobertLundberg@hotmail.com>
2020-08-13 23:54:59 -0700

    Standardized IStatusRepository capitalization

--- Status.Services/StatusRepository.cs
+++ Status.Services/StatusRepository.cs
@@ -15,10 +15,8 @@ namespace Status.Services
     public class StatusRepository : IStatusRepository
     {
         private NewJobsScanThread newJobsScanThread;
-        public IniFileData iniFileData = new IniFileData();
-        public List<StatusMonitorData> monitorData = new List<StatusMonitorData>();
-        public List<StatusData> statusList = new List<StatusData>();
-        private StatusData statusData = new StatusData();
+        public IniFileData IniData = new IniFileData();
+        public List<StatusData> StatusDataList = new List<StatusData>();
         public readonly ILogger<StatusRepository> Logger;
 
         /// <summary>
@@ -45,47 +43,47 @@ namespace Status.Services
 
             // Get information from the Config.ini file
             var IniParser = new IniFileHandler(IniFileName);
-            iniFileData.IniFileName = IniFileName;
-            iniFileData.InputDir = IniParser.Read("Paths", "Input");
-            iniFileData.ProcessingDir = IniParser.Read("Paths", "Processing");
-            iniFileData.RepositoryDir = IniParser.Read("Paths", "Repository");
-            iniFileData.FinishedDir = IniParser.Read("Paths", "Finished");
-            iniFileData.ErrorDir = IniParser.Read("Paths", "Error");
-            iniFileData.ModelerRootDir = IniParser.Read("Paths", "ModelerRootDir");
-            iniFileData.CPUCores = Int32.Parse(IniParser.Read("Process", "CPUCores"));
-            iniFileData.ExecutionLimit = Int32.Parse(IniParser.Read("Process", "ExecutionLimit"));
-            iniFileData.StartPort = Int32.Parse(IniParser.Read("Process", "StartPort"));
-            iniFileData.StatusLogFile = IniParser.Read("Process", "StatusLogFile");
-            iniFileData.ProcessLogFile = IniParser.Read("Process", "ProcessLogFile");
+            IniData.IniFileName = IniFileName;
+            IniData.InputDir = IniParser.Read("Paths", "Input");
+            IniData.ProcessingDir = IniParser.Read("Paths", "Processing");
+            IniData.RepositoryDir = IniParser.Read("Paths", "Repository");
+            IniData.FinishedDir = IniParser.Read("Paths", "Finished");
+            IniData.ErrorDir = IniParser.Read("Paths", "Error");
+            IniData.ModelerRootDir = IniParser.Read("Paths", "ModelerRootDir");
+            IniData.CPUCores = Int32.Parse(IniParser.Read("Process", "CPUCores"));
+            IniData.ExecutionLimit = Int32.Parse(IniParser.Read("Process", "ExecutionLimit"));
+            IniData.StartPort = Int32.Parse(IniParser.Read("Process", "StartPort"));
+            IniData.StatusLogFile = IniParser.Read("Process", "StatusLogFile");
+            IniData.ProcessLogFile = IniParser.Read("Process", "ProcessLogFile");
             string scanTime = IniParser.Read("Process", "ScanTime");
-            iniFileData.ScanTime = Int32.Parse(scanTime.Substring(0, scanTime.IndexOf("#")));
+            IniData.ScanTime = Int32.Parse(scanTime.Substring(0, scanTime.IndexOf("#")));
             string timeLimitString = IniParser.Read("Process", "MaxTimeLimit");
-            iniFileData.MaxTimeLimit = Int32.Parse(timeLimitString.Substring(0, timeLimitString.IndexOf("#")));
+            IniData.MaxTimeLimit = Int32.Parse(timeLimitString.Substring(0, timeLimitString.IndexOf("#")));
             string logFileHistory = IniParser.Read("Process", "LogFileHistory");
-            iniFileData.LogFileHistory = Int32.Parse(logFileHistory.Substring(0, logFileHistory.IndexOf("#")));
+            IniData.LogFileHistory = Int32.Parse(logFileHistory.Substring(0, logFileHistory.IndexOf("#")));
             string logFileMaxSize = IniParser.Read("Process", "logFileMaxSize");
-            iniFileData.LogFileMaxSize = Int32.Parse(logFileMaxSize.Substring(0, logFileMaxSize.IndexOf("#")));
+            IniData.LogFileMaxSize = Int32.Parse(logFileMaxSize.Substring(0, logFileMaxSize.IndexOf("#")));
 
             // Set the log file max size
-            StaticData.logFileSizeLimit = iniFileData.LogFileMaxSize;
+            StaticData.logFileSizeLimit = IniData.LogFileMaxSize;
 
-            string logFile = iniFileData.ProcessLogFile;
+            string logFile = IniData.ProcessLogFile;
             StaticData.Log(logFile, "\nConfig.ini data found:\n");
-            StaticData.Log(logFile, "Input Dir             = " + iniFileData.InputDir);
-            StaticData.Log(logFile, "Processing Dir        = " + iniFileData.ProcessingDir);
-            StaticData.Log(logFile, "Repository Dir        = " + iniFileData.RepositoryDir);
-            StaticData.Log(logFile, "Finished Dir          = " + iniFileData.FinishedDir);
-            StaticData.Log(logFile, "Error Dir             = " + iniFileData.ErrorDir);
-            StaticData.Log(logFile, "Modeler Root Dir      = " + iniFileData.ModelerRootDir);
-            StaticData.Log(logFile, "Status Log File       = " + iniFileData.StatusLogFile);
-            StaticData.Log(logFile, "Process Log File      = " + iniFileData.ProcessLogFile);
-            StaticData.Log(logFile, "CPU Cores             = " + iniFileData.CPUCores + " Cores");
-            StaticData.Log(logFile, "Execution Limit       = " + iniFileData.ExecutionLimit + " Jobs");
-            StaticData.Log(logFile, "Start Port            = " + iniFileData.StartPort);
-            StaticData.Log(logFile, "Scan Time             = " + iniFileData.ScanTime + " Miliseconds");
-            StaticData.Log(logFile, "Max Time Limit        = " + iniFileData.MaxTimeLimit + " Seconds");
-            StaticData.Log(logFile, "Log File History      = " + iniFileData.LogFileHistory + " Days");
-            StaticData.Log(logFile, "Log File Max Size     = " + iniFileData.LogFileMaxSize + " MegaBytes");
+            StaticData.Log(logFile, "Input Dir             = " + IniData.InputDir);
+            StaticData.Log(logFile, "Processing Dir        = " + IniData.ProcessingDir);
+            StaticData.Log(logFile, "Repository Dir        = " + IniData.RepositoryDir);
+            StaticData.Log(logFile, "Finished Dir          = " + IniData.FinishedDir);
+            StaticData.Log(logFile, "Error Dir             = " + IniData.ErrorDir);
+            StaticData.Log(logFile, "Modeler Root Dir      = " + IniData.ModelerRootDir);
+            StaticData.Log(logFile, "Status Log File       = " + IniData.StatusLogFile);
+            StaticData.Log(logFile, "Process Log File      = " + IniData.ProcessLogFile);
+            StaticData.Log(logFile, "CPU Cores             = " + IniData.CPUCores + " Cores");
+            StaticData.Log(logFile, "Execution Limit       = " + IniData.ExecutionLimit + " Jobs");
+            StaticData.Log(logFile, "Start Port            = " + IniData.StartPort);
+            StaticData.Log(logFile, "Scan Time             = " + IniData.ScanTime + " Miliseconds");
+            StaticData.Log(logFile, "Max Time Limit        = " + IniData.MaxTimeLimit + " Seconds");
+            StaticData.Log(logFile, "Log File History      = " + IniData.LogFileHistory + " Days");
+            StaticData.Log(logFile, "Log File Max Size     = " + IniData.LogFileMaxSize + " MegaBytes");
         }
 
         /// <summary>
@@ -98,7 +96,7 @@ namespace Status.Services
             {
                 Logger.LogError("Log File History status failed to instantiate");
             }
-            status.CheckLogFileHistory(iniFileData.StatusLogFile, iniFileData.LogFileHistory, logger);
+            status.CheckLogFileHistory(IniData.StatusLogFile, IniData.LogFileHistory, Logger);
         }
 
         /// <summary>
@@ -118,7 +116,7 @@ namespace Status.Services
             StaticData.ShutdownFlag = false;
 
             // Start thread to scan for old then new jobs
-            newJobsScanThread = new NewJobsScanThread(iniFileData, statusList, logger);
+            newJobsScanThread = new NewJobsScanThread(IniData, StatusDataList, Logger);
             if (newJobsScanThread == null)
             {
                 Logger.LogError("StartMonitorProcess newJobsScanThread failed to instantiate");
@@ -132,7 +130,7 @@ namespace Status.Services
         /// <returns>Status Data List</returns>
         public IEnumerable<StatusData> GetJobStatus()
         {
-            return statusList;
+            return StatusDataList;
         }
 
         /// <summary>
@@ -141,14 +139,14 @@ namespace Status.Services
         /// <returns>History Status Data List</returns>
         public IEnumerable<StatusData> GetHistoryData()
         {
-            List<StatusData> statusList = new List<StatusData>();
+            List<StatusData> StatusDataList = new List<StatusData>();
             StatusEntry status = new StatusEntry();
-            statusList = status.ReadFromCsvFile(iniFileData.StatusLogFile, iniFileData, logger);
-            if (statusList == null)
+            StatusDataList = status.ReadFromCsvFile(IniData.StatusLogFile, IniData, Logger);
+            if (StatusDataList == null)
             {
-                Logger.LogError("GetHistoryData statusList failed to instantiate");
+                Logger.LogError("GetHistoryData StatusDataList failed to instantiate");
             }
-            return statusList;
+            return StatusDataList;
         }
     }
 }
