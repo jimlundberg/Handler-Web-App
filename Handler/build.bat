3f5b26114737c539cfa96225b94ff7b299b0cafb
JamesRobertLundberg <JamesRobertLundberg@hotmail.com>
2020-08-28 12:15:33 -0700

    Changed build.bat to create an x64 release build and copy the files from correct location

--- Handler/build.bat
+++ Handler/build.bat
@@ -1,6 +1,5 @@
 cd C:\SSMCharacterizationHandler\Handler
-DOTNET build handler
-XCOPY /S /Q /H /R /Y C:\SSMCharacterizationHandler\Handler\Handler\Handler\bin\Release\netcoreapp3.1 C:\SSMCharacterizationHandler\Handler
-XCOPY /S /I /Q /H /R /Y C:\SSMCharacterizationHandler\Handler\Handler\Handler\wwwroot C:\SSMCharacterizationHandler\Handler\wwwroot
+dotnet publish -c "Release" -r "win-x64" handler
+XCOPY /S /Q /H /R /Y C:\SSMCharacterizationHandler\Handler\Handler\Handler\bin\Release\netcoreapp3.1\win-x64\publish C:\SSMCharacterizationHandler\Handler
 copy C:\SSMCharacterizationHandler\Handler\Handler\Handler\Config.ini C:\SSMCharacterizationHandler\Handler
 cd ..
