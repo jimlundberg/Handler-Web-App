fafd4de939d600ecb609b7da563adcf6d0518d4e
JamesRobertLundberg <JamesRobertLundberg@hotmail.com>
2020-08-28 18:05:38 -0700

    Added delete of all but the Handler build directory

--- Handler/build.bat
+++ Handler/build.bat
@@ -1,5 +1,19 @@
+@echo off
+
 cd C:\SSMCharacterizationHandler\Handler
+
+REM Delete all but the Handler directory
+pushd "C:\SSMCharacterizationHandler\Handler" || exit /B 1
+for /D %%D in ("*") do (
+    if /I not "%%~nxD"=="Handler" rd /S /Q "%%~D"
+)
+for %%F in ("*") do (
+    del /Q "%%~F"
+)
+popd
+
 dotnet publish -c "Release" -r "win-x64" handler
 XCOPY /S /Q /H /R /Y C:\SSMCharacterizationHandler\Handler\Handler\Handler\bin\Release\netcoreapp3.1\win-x64\publish C:\SSMCharacterizationHandler\Handler
 copy C:\SSMCharacterizationHandler\Handler\Handler\Handler\Config.ini C:\SSMCharacterizationHandler\Handler
+rmdir /Q /S C:\SSMCharacterizationHandler\Handler\Handler\Handler\bin
 cd ..
