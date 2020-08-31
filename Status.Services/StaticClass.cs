e7838db9c444f8240ad71a2fd3a594e36c28027f
JamesRobertLundberg <JamesRobertLundberg@hotmail.com>
2020-08-31 14:54:12 -0700

    Don't need the thread handle then

--- Status.Services/StaticClass.cs
+++ Status.Services/StaticClass.cs
@@ -27,7 +27,6 @@ namespace Status.Services
         public static Thread InputFileWatcherThreadHandle;
         public static Thread JobRunThreadHandle;
         public static Thread TcpIpListenThreadHandle;
-        public static Thread CommandLineThreadHandle;
 
         public static volatile bool ShutdownFlag = false;
         public static volatile bool PauseFlag = false;
