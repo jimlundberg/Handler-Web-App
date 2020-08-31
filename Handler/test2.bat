ddb1a9038ee66bd83e49cf387595286fc9f90502
JamesRobertLundberg <JamesRobertLundberg@hotmail.com>
2020-08-30 17:58:23 -0700

    Improved test2.bat

--- Handler/test2.bat
+++ Handler/test2.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
 REM Handler App test batch file to:
-REM 1. Copies the job 75300037D00.xml files
+REM 1. Copies the job 75300037D00.xml files from 
 REM 2. Copies the job .csv and .tab files into the directory to simulate finishing job Input
 REM 3. Waits until each job directory is created in the ProcessingBuffer (by Handler App)
 REM 4. Then copies the files that complete a job into the ProcessingBuffer directory
@@ -12,10 +12,8 @@ set buffer=Input Buffer
 REM set buffer=ProcessingBuffer
 set DirType=Pass
 REM set DirType=Fail
-set DELAY=10
 
-REM Copy complete start directories into the Input Buffer
-
+:RunTest
 
 SET JOB=1185840_202003250942
 echo Copying %JOB%
@@ -29,20 +27,15 @@ xcopy /S /I /Q "test\%JOB%" "ProcessingBuffer\%JOB%"
 
 timeout 10
 
-SET JOB=1278061_202006301109
+SET JOB=1178350_202006030856
 echo Copying %JOB%
-xcopy /S /I /Q "test\%JOB%" "%buffer%\%JOB%"
+xcopy /S /I /Q "test\%JOB%" "ProcessingBuffer\%JOB%"
 
 timeout 10
 
-SET JOB=1202741_202003101418
+SET JOB=1202740_202006110832
 echo Copying %JOB%
 xcopy /S /I /Q "test\%JOB%" "%buffer%\%JOB%"
-
-
-GOTO End
-
-
 
 timeout 10
 
@@ -52,7 +45,25 @@ xcopy /S /I /Q "test\%JOB%" "%buffer%\%JOB%"
 
 timeout 10
 
+SET JOB=1202741_202003101418
+echo Copying %JOB%
+xcopy /S /I /Q "test\%JOB%" "%buffer%\%JOB%"
+
+timeout 10
+
 SET JOB=1178352_202005050818
+echo Copying %JOB%
+xcopy /S /I /Q "test\%JOB%" "%buffer%\%JOB%"
+
+timeout 10
+
+SET JOB=1278061_202006301109
+echo Copying %JOB%
+xcopy /S /I /Q "test\%JOB%" "%buffer%\%JOB%"
+
+timeout 10
+
+SET JOB=1610789_201911281057
 echo Copying %JOB%
 xcopy /S /I /Q "test\%JOB%" "%buffer%\%JOB%"
 
