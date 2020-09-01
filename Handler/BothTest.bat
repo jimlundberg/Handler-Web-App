be230526a655bd7d2b48e81462150cb9675d7bd4
JamesRobertLundberg <JamesRobertLundberg@hotmail.com>
2020-09-01 01:25:21 -0700

    Fixed batch file copies

--- Handler/BothTest.bat
+++ Handler/BothTest.bat
@@ -1,8 +1,9 @@
 @ECHO OFF
 
-REM Handler App test batch file to:
-REM 1. Copies the job 75300037D00.xml file with directory into the Input Buffer
-REM 2. Copies the job .csv and .tab files into the directory to stimulate job start
+REM Handler App Both Partial and Full Directory type adds test
+REM 1. Copies some the job 75300037D00.xml file to the Input Buffer Job directory
+REM 2. Then copies the job .csv and .tab files into the directory to simulate finishing job Input
+REM 3. Interspaces Full directory addes between Partial adds
 
 :ConfigureTest
 
@@ -15,10 +16,11 @@ REM set DirType=Fail
 
 SET JOB=1185840_202003250942
 echo Copying %JOB% .xml file
-xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%"
+xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%\*.xml"
 timeout 5
 echo Copying %JOB% other files
-xcopy /S /F "test\%JOB% - Start\*.*" "%buffer%\%JOB%"
+xcopy /S /F "test\%JOB% - Start\*.csv" "%buffer%\%JOB%\*.csv"
+xcopy /S /F "test\%JOB% - Start\*.tab" "%buffer%\%JOB%\*.tab"
 
 timeout 10
 
@@ -37,19 +39,21 @@ timeout 10
 
 SET JOB=1202740_202006110832
 echo Copying %JOB% .xml file
-xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%"
+xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%\*.xml"
 timeout 5
 echo Copying %JOB% other files
-xcopy /S /F "test\%JOB% - Start\*.*" "%buffer%\%JOB%"
+xcopy /S /F "test\%JOB% - Start\*.csv" "%buffer%\%JOB%\*.csv"
+xcopy /S /F "test\%JOB% - Start\*.tab" "%buffer%\%JOB%\*.tab"
 
 timeout 10
 
 SET JOB=1185841_202005070801
 echo Copying %JOB% .xml file
-xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%"
+xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%\*.xml"
 timeout 5
 echo Copying %JOB% other files
-xcopy /S /F "test\%JOB% - Start\*.*" "%buffer%\%JOB%"
+xcopy /S /F "test\%JOB% - Start\*.csv" "%buffer%\%JOB%\*.csv"
+xcopy /S /F "test\%JOB% - Start\*.tab" "%buffer%\%JOB%\*.tab"
 
 timeout 10
 
@@ -61,10 +65,11 @@ timeout 10
 
 SET JOB=1178352_202005050818
 echo Copying %JOB% .xml file
-xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%"
+xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%\*.xml"
 timeout 5
 echo Copying %JOB% other files
-xcopy /S /F "test\%JOB% - Start\*.*" "%buffer%\%JOB%"
+xcopy /S /F "test\%JOB% - Start\*.csv" "%buffer%\%JOB%\*.csv"
+xcopy /S /F "test\%JOB% - Start\*.tab" "%buffer%\%JOB%\*.tab"
 
 timeout 10
 
@@ -76,9 +81,10 @@ timeout 10
 
 SET JOB=1610789_201911281057
 echo Copying %JOB% .xml file
-xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%"
+xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%\*.xml"
 timeout 5
 echo Copying %JOB% other files
-xcopy /S /F "test\%JOB% - Start\*.*" "%buffer%\%JOB%"
+xcopy /S /F "test\%JOB% - Start\*.csv" "%buffer%\%JOB%\*.csv"
+xcopy /S /F "test\%JOB% - Start\*.tab" "%buffer%\%JOB%\*.tab"
 
 :End
--- Handler/PartialTest.bat
+++ Handler/PartialTest.bat
@@ -1,10 +1,8 @@
 @ECHO OFF
 
-REM Handler App test batch file to:
-REM 1. Copies the job 75300037D00.xml files
-REM 2. Copies the job .csv and .tab files into the directory to simulate finishing job Input
-REM 3. Waits until each job directory is created in the ProcessingBuffer (by Handler App)
-REM 4. Then copies the files that complete a job into the ProcessingBuffer directory
+REM Handler App Partial Directory Add test
+REM 1. First copies the job 75300037D00.xml file to the Input Buffer Job directory
+REM 2. Then copies the job .csv and .tab files into the directory to simulate finishing job Input
 
 :ConfigureTest
 
@@ -18,81 +16,89 @@ REM Copy directories into the Input Buffer
 
 SET JOB=1185840_202003250942
 echo Copying %JOB% .xml file
-xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%"
+xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%\*.xml"
 timeout 5
 echo Copying %JOB% other files
-xcopy /S /F "test\%JOB% - Start\*.*" "%buffer%\%JOB%"
+xcopy /S /F "test\%JOB% - Start\*.csv" "%buffer%\%JOB%\*.csv"
+xcopy /S /F "test\%JOB% - Start\*.tab" "%buffer%\%JOB%\*.tab"
 
 timeout 10
 
 SET JOB=1307106_202002181307
 echo Copying %JOB% .xml file
-xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%"
+xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%\*.xml"
 timeout 5
 echo Copying %JOB% other files
-xcopy /S /F "test\%JOB% - Start\*.*" "%buffer%\%JOB%"
+xcopy /S /F "test\%JOB% - Start\*.csv" "%buffer%\%JOB%\*.csv"
+xcopy /S /F "test\%JOB% - Start\*.tab" "%buffer%\%JOB%\*.tab"
 
 timeout 10
 
 SET JOB=1178350_202006030856
 echo Copying %JOB% .xml file
-xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%"
+xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%\*.xml"
 timeout 5
 echo Copying %JOB% other files
-xcopy /S /F "test\%JOB% - Start\*.*" "%buffer%\%JOB%"
+xcopy /S /F "test\%JOB% - Start\*.csv" "%buffer%\%JOB%\*.csv"
+xcopy /S /F "test\%JOB% - Start\*.tab" "%buffer%\%JOB%\*.tab"
 
 timeout 10
 
 SET JOB=1202740_202006110832
 echo Copying %JOB% .xml file
-xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%"
+xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%\*.xml"
 timeout 5
 echo Copying %JOB% other files
-xcopy /S /F "test\%JOB% - Start\*.*" "%buffer%\%JOB%"
+xcopy /S /F "test\%JOB% - Start\*.csv" "%buffer%\%JOB%\*.csv"
+xcopy /S /F "test\%JOB% - Start\*.tab" "%buffer%\%JOB%\*.tab"
 
 timeout 10
 
 SET JOB=1185841_202005070801
 echo Copying %JOB% .xml file
-xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%"
+xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%\*.xml"
 timeout 5
 echo Copying %JOB% other files
-xcopy /S /F "test\%JOB% - Start\*.*" "%buffer%\%JOB%"
+xcopy /S /F "test\%JOB% - Start\*.csv" "%buffer%\%JOB%\*.csv"
+xcopy /S /F "test\%JOB% - Start\*.tab" "%buffer%\%JOB%\*.tab"
 
 timeout 10
 
 SET JOB=1202741_202003101418
 echo Copying %JOB% .xml file
-xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%"
+xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%\*.xml"
 timeout 5
 echo Copying %JOB% other files
-xcopy /S /F "test\%JOB% - Start\*.*" "%buffer%\%JOB%"
+xcopy /S /F "test\%JOB% - Start\*.csv" "%buffer%\%JOB%\*.csv"
+xcopy /S /F "test\%JOB% - Start\*.tab" "%buffer%\%JOB%\*.tab"
 
 timeout 10
 
 SET JOB=1178352_202005050818
 echo Copying %JOB% .xml file
-xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%"
+xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%\*.xml"
 timeout 5
 echo Copying %JOB% other files
-xcopy /S /F "test\%JOB% - Start\*.*" "%buffer%\%JOB%"
+xcopy /S /F "test\%JOB% - Start\*.csv" "%buffer%\%JOB%\*.csv"
+xcopy /S /F "test\%JOB% - Start\*.tab" "%buffer%\%JOB%\*.tab"
 
 timeout 10
 
 SET JOB=1278061_202006301109
 echo Copying %JOB% .xml file
-xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%"
+xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%\*.xml"
 timeout 5
 echo Copying %JOB% other files
-xcopy /S /F "test\%JOB% - Start\*.*" "%buffer%\%JOB%"
+xcopy /S /F "test\%JOB% - Start\*.csv" "%buffer%\%JOB%\*.csv"
+xcopy /S /F "test\%JOB% - Start\*.tab" "%buffer%\%JOB%\*.tab"
 
 timeout 10
 
 SET JOB=1610789_201911281057
-echo Copying %JOB% .xml file
-xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%"
+xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%\*.xml"
 timeout 5
 echo Copying %JOB% other files
-xcopy /S /F "test\%JOB% - Start\*.*" "%buffer%\%JOB%"
+xcopy /S /F "test\%JOB% - Start\*.csv" "%buffer%\%JOB%\*.csv"
+xcopy /S /F "test\%JOB% - Start\*.tab" "%buffer%\%JOB%\*.tab"
 
 :End
