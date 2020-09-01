1cd83dc155e9aea6db43686e92b66b8ebe721aa5
JamesRobertLundberg <JamesRobertLundberg@hotmail.com>
2020-09-01 14:12:08 -0700

    Added delete of the Handler log files

--- Handler/Clean.bat
+++ Handler/Clean.bat
@@ -1,6 +1,6 @@
-:ClearWorkingDirectories
+@echo off
 
-@ECHO OFF
+:ClearWorkingDirectories
 
 cd "ProcessingBuffer"
 del /F /Q /S *.* >nul 2>&1
@@ -27,4 +27,11 @@ del /F /Q /S *. >nul 2>&1
 rd /s /q . >nul 2>&1
 cd..
 
+:DeleteLogFiles
+
+del /F /Q /S Handler\log.txt >nul 2>&1
+del /F /Q /S Handler\StatusData.csv >nul 2>&1
+
 cls
+
+:End
