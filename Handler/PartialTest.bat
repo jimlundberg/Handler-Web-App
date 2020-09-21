2523bb03e2565821eca73c3497f1cee0012ecd8a
JamesRobertLundberg <JamesRobertLundberg@hotmail.com>
2020-09-21 05:43:56 -0700

    Improved tests

--- Handler/PartialTest.bat
+++ Handler/PartialTest.bat
@@ -15,145 +15,145 @@ set DELAY=10
 REM Copy directories into the Input Buffer
 
 SET JOB=1185840_202003250942
-echo Copying Job start files one at a time
-xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%\*.xml"
+echo Copying Job %JOB% files one at a time
+SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
 timeout 5
-xcopy /S /F "test\%JOB%\*mode0.csv" "%buffer%\%JOB%\*mode0.csv"
+SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
 timeout 5
-xcopy /S /F "test\%JOB%\*mode1.csv" "%buffer%\%JOB%\*mode1.csv"
+SlowCopy "test\%JOB%\%JOB%_mode2.csv" "%buffer%\%JOB%\%JOB%_mode2.csv"
 timeout 5
-xcopy /S /F "test\%JOB%\*mode2.csv" "%buffer%\%JOB%\*mode2.csv"
+SlowCopy "test\%JOB%\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
 timeout 5
-xcopy /S /F "test\%JOB%\*Cap_Template.tab" "%buffer%\%JOB%\*Cap_Template.tab"
+SlowCopy "test\%JOB%\Cap_Template.tab" "%buffer%\%JOB%\Cap_Template.tab"
 timeout 5
-xcopy /S /F "test\%JOB%\*Tune_Template.tab" "%buffer%\%JOB%\*Tune_Template.tab"
+SlowCopy "test\%JOB%\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
 
 timeout 10
 
 SET JOB=1307106_202002181307
-echo Copying Job start files one at a time
-xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%\*.xml"
+echo Copying Job %JOB% files one at a time
+SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
 timeout 5
-xcopy /S /F "test\%JOB%\*mode0.csv" "%buffer%\%JOB%\*mode0.csv"
+SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
 timeout 5
-xcopy /S /F "test\%JOB%\*mode1.csv" "%buffer%\%JOB%\*mode1.csv"
+SlowCopy "test\%JOB%\%JOB%_mode2.csv" "%buffer%\%JOB%\%JOB%_mode2.csv"
 timeout 5
-xcopy /S /F "test\%JOB%\*mode2.csv" "%buffer%\%JOB%\*mode2.csv"
+SlowCopy "test\%JOB%\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
 timeout 5
-xcopy /S /F "test\%JOB%\*Cap_Template.tab" "%buffer%\%JOB%\*Cap_Template.tab"
+SlowCopy "test\%JOB%\Cap_Template.tab" "%buffer%\%JOB%\Cap_Template.tab"
 timeout 5
-xcopy /S /F "test\%JOB%\*Tune_Template.tab" "%buffer%\%JOB%\*Tune_Template.tab"
+SlowCopy "test\%JOB%\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
 
 timeout 10
 
 SET JOB=1178350_202006030856
-echo Copying Job start files one at a time
-xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%\*.xml"
+echo Copying Job %JOB% files one at a time
+SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
 timeout 5
-xcopy /S /F "test\%JOB%\*mode0.csv" "%buffer%\%JOB%\*mode0.csv"
+SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
 timeout 5
-xcopy /S /F "test\%JOB%\*mode1.csv" "%buffer%\%JOB%\*mode1.csv"
+SlowCopy "test\%JOB%\%JOB%_mode2.csv" "%buffer%\%JOB%\%JOB%_mode2.csv"
 timeout 5
-xcopy /S /F "test\%JOB%\*mode2.csv" "%buffer%\%JOB%\*mode2.csv"
+SlowCopy "test\%JOB%\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
 timeout 5
-xcopy /S /F "test\%JOB%\*Cap_Template.tab" "%buffer%\%JOB%\*Cap_Template.tab"
+SlowCopy "test\%JOB%\Cap_Template.tab" "%buffer%\%JOB%\Cap_Template.tab"
 timeout 5
-xcopy /S /F "test\%JOB%\*Tune_Template.tab" "%buffer%\%JOB%\*Tune_Template.tab"
+SlowCopy "test\%JOB%\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
 
 timeout 10
 
 SET JOB=1202740_202006110832
-echo Copying Job start files one at a time
-xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%\*.xml"
+echo Copying Job %JOB% files one at a time
+SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
 timeout 5
-xcopy /S /F "test\%JOB%\*mode0.csv" "%buffer%\%JOB%\*mode0.csv"
+SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
 timeout 5
-xcopy /S /F "test\%JOB%\*mode1.csv" "%buffer%\%JOB%\*mode1.csv"
+SlowCopy "test\%JOB%\%JOB%_mode2.csv" "%buffer%\%JOB%\%JOB%_mode2.csv"
 timeout 5
-xcopy /S /F "test\%JOB%\*mode2.csv" "%buffer%\%JOB%\*mode2.csv"
+SlowCopy "test\%JOB%\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
 timeout 5
-xcopy /S /F "test\%JOB%\*Cap_Template.tab" "%buffer%\%JOB%\*Cap_Template.tab"
+SlowCopy "test\%JOB%\Cap_Template.tab" "%buffer%\%JOB%\Cap_Template.tab"
 timeout 5
-xcopy /S /F "test\%JOB%\*Tune_Template.tab" "%buffer%\%JOB%\*Tune_Template.tab"
+SlowCopy "test\%JOB%\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
 
 timeout 10
 
 SET JOB=1185841_202005070801
-echo Copying Job start files one at a time
-xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%\*.xml"
+echo Copying Job %JOB% files one at a time
+SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
 timeout 5
-xcopy /S /F "test\%JOB%\*mode0.csv" "%buffer%\%JOB%\*mode0.csv"
+SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
 timeout 5
-xcopy /S /F "test\%JOB%\*mode1.csv" "%buffer%\%JOB%\*mode1.csv"
+SlowCopy "test\%JOB%\%JOB%_mode2.csv" "%buffer%\%JOB%\%JOB%_mode2.csv"
 timeout 5
-xcopy /S /F "test\%JOB%\*mode2.csv" "%buffer%\%JOB%\*mode2.csv"
+SlowCopy "test\%JOB%\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
 timeout 5
-xcopy /S /F "test\%JOB%\*Cap_Template.tab" "%buffer%\%JOB%\*Cap_Template.tab"
+SlowCopy "test\%JOB%\Cap_Template.tab" "%buffer%\%JOB%\Cap_Template.tab"
 timeout 5
-xcopy /S /F "test\%JOB%\*Tune_Template.tab" "%buffer%\%JOB%\*Tune_Template.tab"
+SlowCopy "test\%JOB%\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
 
 timeout 10
 
 SET JOB=1202741_202003101418
-echo Copying Job start files one at a time
-xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%\*.xml"
+echo Copying Job %JOB% files one at a time
+SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
 timeout 5
-xcopy /S /F "test\%JOB%\*mode0.csv" "%buffer%\%JOB%\*mode0.csv"
+SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
 timeout 5
-xcopy /S /F "test\%JOB%\*mode1.csv" "%buffer%\%JOB%\*mode1.csv"
+SlowCopy "test\%JOB%\%JOB%_mode2.csv" "%buffer%\%JOB%\%JOB%_mode2.csv"
 timeout 5
-xcopy /S /F "test\%JOB%\*mode2.csv" "%buffer%\%JOB%\*mode2.csv"
+SlowCopy "test\%JOB%\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
 timeout 5
-xcopy /S /F "test\%JOB%\*Cap_Template.tab" "%buffer%\%JOB%\*Cap_Template.tab"
+SlowCopy "test\%JOB%\Cap_Template.tab" "%buffer%\%JOB%\Cap_Template.tab"
 timeout 5
-xcopy /S /F "test\%JOB%\*Tune_Template.tab" "%buffer%\%JOB%\*Tune_Template.tab"
+SlowCopy "test\%JOB%\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
 
 timeout 10
 
 SET JOB=1178352_202005050818
-echo Copying Job start files one at a time
-xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%\*.xml"
+echo Copying Job %JOB% files one at a time
+SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
 timeout 5
-xcopy /S /F "test\%JOB%\*mode0.csv" "%buffer%\%JOB%\*mode0.csv"
+SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
 timeout 5
-xcopy /S /F "test\%JOB%\*mode1.csv" "%buffer%\%JOB%\*mode1.csv"
+SlowCopy "test\%JOB%\%JOB%_mode2.csv" "%buffer%\%JOB%\%JOB%_mode2.csv"
 timeout 5
-xcopy /S /F "test\%JOB%\*mode2.csv" "%buffer%\%JOB%\*mode2.csv"
+SlowCopy "test\%JOB%\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
 timeout 5
-xcopy /S /F "test\%JOB%\*Cap_Template.tab" "%buffer%\%JOB%\*Cap_Template.tab"
+SlowCopy "test\%JOB%\Cap_Template.tab" "%buffer%\%JOB%\Cap_Template.tab"
 timeout 5
-xcopy /S /F "test\%JOB%\*Tune_Template.tab" "%buffer%\%JOB%\*Tune_Template.tab"
+SlowCopy "test\%JOB%\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
 
 timeout 10
 
 SET JOB=1278061_202006301109
-echo Copying Job start files one at a time
-xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%\*.xml"
+echo Copying Job %JOB% files one at a time
+SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
 timeout 5
-xcopy /S /F "test\%JOB%\*mode0.csv" "%buffer%\%JOB%\*mode0.csv"
+SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
 timeout 5
-xcopy /S /F "test\%JOB%\*mode1.csv" "%buffer%\%JOB%\*mode1.csv"
+SlowCopy "test\%JOB%\%JOB%_mode2.csv" "%buffer%\%JOB%\%JOB%_mode2.csv"
 timeout 5
-xcopy /S /F "test\%JOB%\*mode2.csv" "%buffer%\%JOB%\*mode2.csv"
+SlowCopy "test\%JOB%\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
 timeout 5
-xcopy /S /F "test\%JOB%\*Cap_Template.tab" "%buffer%\%JOB%\*Cap_Template.tab"
+SlowCopy "test\%JOB%\Cap_Template.tab" "%buffer%\%JOB%\Cap_Template.tab"
 timeout 5
-xcopy /S /F "test\%JOB%\*Tune_Template.tab" "%buffer%\%JOB%\*Tune_Template.tab"
+SlowCopy "test\%JOB%\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
 
 timeout 10
 
 SET JOB=1610789_201911281057
-echo Copying Job start files one at a time
-xcopy /S /F "test\%JOB%\*.xml" "%buffer%\%JOB%\*.xml"
+echo Copying Job %JOB% files one at a time
+SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
 timeout 5
-xcopy /S /F "test\%JOB%\*mode0.csv" "%buffer%\%JOB%\*mode0.csv"
+SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
 timeout 5
-xcopy /S /F "test\%JOB%\*mode1.csv" "%buffer%\%JOB%\*mode1.csv"
+SlowCopy "test\%JOB%\%JOB%_mode2.csv" "%buffer%\%JOB%\%JOB%_mode2.csv"
 timeout 5
-xcopy /S /F "test\%JOB%\*mode2.csv" "%buffer%\%JOB%\*mode2.csv"
+SlowCopy "test\%JOB%\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
 timeout 5
-xcopy /S /F "test\%JOB%\*Cap_Template.tab" "%buffer%\%JOB%\*Cap_Template.tab"
+SlowCopy "test\%JOB%\Cap_Template.tab" "%buffer%\%JOB%\Cap_Template.tab"
 timeout 5
-xcopy /S /F "test\%JOB%\*Tune_Template.tab" "%buffer%\%JOB%\*Tune_Template.tab"
+SlowCopy "test\%JOB%\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
 
 :End
