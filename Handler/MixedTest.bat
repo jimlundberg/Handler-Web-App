9edf89939ea662100d9c067e6df0884f232dade3
JamesRobertLundberg <JamesRobertLundberg@hotmail.com>
2020-09-21 05:14:32 -0700

    Edited test files

--- Handler/MixedTest.bat
+++ Handler/MixedTest.bat
@@ -13,7 +13,7 @@ REM set buffer=ProcessingBuffer
 :RunTest
 
 SET JOB=1185840_202003250942
-echo Copying Job start files one at a time
+echo Copying %JOB% Job files one at a time
 SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
 timeout 5
 SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
@@ -29,20 +29,29 @@ SlowCopy "test\%JOB%\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
 timeout 10
 
 SET JOB=1307106_202002181307
-echo Copying %JOB%
+echo Copying %JOB% Complete directory
 xcopy /S /I /Q "test\%JOB%" "%buffer%\%JOB%"
 
 timeout 10
 
 SET JOB=1178350_202006030856
-echo Copying %JOB%
-xcopy /S /I /Q "test\%JOB%" "%buffer%\%JOB%"
-
+echo Copying Job %JOB% files one at a time
+SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
+timeout 5
+SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
+timeout 5
+SlowCopy "test\%JOB%\%JOB%_mode2.csv" "%buffer%\%JOB%\%JOB%_mode2.csv"
+timeout 5
+SlowCopy "test\%JOB%\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
+timeout 5
+SlowCopy "test\%JOB%\Cap_Template.tab" "%buffer%\%JOB%\Cap_Template.tab"
+timeout 5
+SlowCopy "test\%JOB%\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
 
 timeout 10
 
 SET JOB=1202740_202006110832
-echo Copying Job start files one at a time
+echo Copying %JOB% Job files one at a time
 SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
 timeout 5
 SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
@@ -58,7 +67,7 @@ SlowCopy "test\%JOB%\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
 timeout 10
 
 SET JOB=1185841_202005070801
-echo Copying Job start files one at a time
+echo Copying %JOB% Job files one at a time
 SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
 timeout 5
 SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
@@ -74,13 +83,13 @@ SlowCopy "test\%JOB%\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
 timeout 10
 
 SET JOB=1202741_202003101418
-echo Copying %JOB%
+echo Copying %JOB% Complete directory
 xcopy /S /I /Q "test\%JOB%" "%buffer%\%JOB%"
 
 timeout 10
 
 SET JOB=1178352_202005050818
-echo Copying Job start files one at a time
+echo Copying %JOB% Job files one at a time
 SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
 timeout 5
 SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
@@ -96,23 +105,13 @@ SlowCopy "test\%JOB%\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
 timeout 10
 
 SET JOB=1278061_202006301109
-echo Copying %JOB%
+echo Copying %JOB% Complete directory
 xcopy /S /I /Q "test\%JOB%" "%buffer%\%JOB%"
 
 timeout 10
 
 SET JOB=1610789_201911281057
-echo Copying Job start files one at a time
-SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
-timeout 5
-SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
-timeout 5
-SlowCopy "test\%JOB%\%JOB%_mode2.csv" "%buffer%\%JOB%\%JOB%_mode2.csv"
-timeout 5
-SlowCopy "test\%JOB%\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
-timeout 5
-SlowCopy "test\%JOB%\Cap_Template.tab" "%buffer%\%JOB%\Cap_Template.tab"
-timeout 5
-SlowCopy "test\%JOB%\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
+echo Copying %JOB% Complete directory
+xcopy /S /I /Q "test\%JOB%" "%buffer%\%JOB%"
 
 :End
--- Handler/PartialTest.bat
+++ Handler/PartialTest.bat
@@ -15,7 +15,7 @@ set DELAY=10
 REM Copy directories into the Input Buffer
 
 SET JOB=1185840_202003250942
-echo Copying Job start files one at a time
+echo Copying Job %JOB% files one at a time
 SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
 timeout 5
 SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
@@ -31,7 +31,7 @@ SlowCopy "test\%JOB%\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
 timeout 10
 
 SET JOB=1307106_202002181307
-echo Copying Job start files one at a time
+echo Copying Job %JOB% files one at a time
 SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
 timeout 5
 SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
@@ -47,7 +47,7 @@ SlowCopy "test\%JOB%\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
 timeout 10
 
 SET JOB=1178350_202006030856
-echo Copying Job start files one at a time
+echo Copying Job %JOB% files one at a time
 SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
 timeout 5
 SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
@@ -63,7 +63,7 @@ SlowCopy "test\%JOB%\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
 timeout 10
 
 SET JOB=1202740_202006110832
-echo Copying Job start files one at a time
+echo Copying Job %JOB% files one at a time
 SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
 timeout 5
 SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
@@ -79,7 +79,7 @@ SlowCopy "test\%JOB%\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
 timeout 10
 
 SET JOB=1185841_202005070801
-echo Copying Job start files one at a time
+echo Copying Job %JOB% files one at a time
 SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
 timeout 5
 SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
@@ -95,7 +95,7 @@ SlowCopy "test\%JOB%\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
 timeout 10
 
 SET JOB=1202741_202003101418
-echo Copying Job start files one at a time
+echo Copying Job %JOB% files one at a time
 SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
 timeout 5
 SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
@@ -111,7 +111,7 @@ SlowCopy "test\%JOB%\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
 timeout 10
 
 SET JOB=1178352_202005050818
-echo Copying Job start files one at a time
+echo Copying Job %JOB% files one at a time
 SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
 timeout 5
 SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
@@ -127,7 +127,7 @@ SlowCopy "test\%JOB%\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
 timeout 10
 
 SET JOB=1278061_202006301109
-echo Copying Job start files one at a time
+echo Copying Job %JOB% files one at a time
 SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
 timeout 5
 SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
@@ -143,7 +143,7 @@ SlowCopy "test\%JOB%\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
 timeout 10
 
 SET JOB=1610789_201911281057
-echo Copying Job start files one at a time
+echo Copying Job %JOB% files one at a time
 SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
 timeout 5
 SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
