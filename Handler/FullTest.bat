c9331ec6aeddaeaa51bdf57630ea316cc96de2e7
JamesRobertLundberg <JamesRobertLundberg@hotmail.com>
2020-09-26 10:02:54 -0700

    Changed to 110% and removed table call

--- Handler/FullTest.bat
+++ Handler/FullTest.bat
@@ -14,62 +14,53 @@ REM set DirType=Fail
 SET JOB=1185840_202003250942
 echo Copying %JOB%
 xcopy /S /I /Q "test\%JOB%" "%buffer%\%JOB%"
-xcopy /S /I /Q "test\%JOB% - Start" "%buffer%\%JOB%"
 
 timeout 10
 
 SET JOB=1307106_202002181307
 echo Copying %JOB%
 xcopy /S /I /Q "test\%JOB%" "%buffer%\%JOB%"
-xcopy /S /I /Q "test\%JOB% - Start" "%buffer%\%JOB%"
 
 timeout 10
 
 SET JOB=1178350_202006030856
 echo Copying %JOB%
 xcopy /S /I /Q "test\%JOB%" "%buffer%\%JOB%"
-xcopy /S /I /Q "test\%JOB% - Start" "%buffer%\%JOB%"
 
 timeout 10
 
 SET JOB=1202740_202006110832
 echo Copying %JOB%
 xcopy /S /I /Q "test\%JOB%" "%buffer%\%JOB%"
-xcopy /S /I /Q "test\%JOB% - Start" "%buffer%\%JOB%"
 
 timeout 10
 
 SET JOB=1185841_202005070801
 echo Copying %JOB%
 xcopy /S /I /Q "test\%JOB%" "%buffer%\%JOB%"
-xcopy /S /I /Q "test\%JOB% - Start" "%buffer%\%JOB%"
 
 timeout 10
 
 SET JOB=1202741_202003101418
 echo Copying %JOB%
 xcopy /S /I /Q "test\%JOB%" "%buffer%\%JOB%"
-xcopy /S /I /Q "test\%JOB% - Start" "%buffer%\%JOB%"
 
 timeout 10
 
 SET JOB=1178352_202005050818
 echo Copying %JOB%
 xcopy /S /I /Q "test\%JOB%" "%buffer%\%JOB%"
-xcopy /S /I /Q "test\%JOB% - Start" "%buffer%\%JOB%"
 
 timeout 10
 
 SET JOB=1278061_202006301109
 echo Copying %JOB%
 xcopy /S /I /Q "test\%JOB%" "%buffer%\%JOB%"
-xcopy /S /I /Q "test\%JOB% - Start" "%buffer%\%JOB%"
 
 timeout 10
 
 SET JOB=1610789_201911281057
 echo Copying %JOB%
 xcopy /S /I /Q "test\%JOB%" "%buffer%\%JOB%"
-xcopy /S /I /Q "test\%JOB% - Start" "%buffer%\%JOB%"
 
 :End
