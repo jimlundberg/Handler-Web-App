0bad54541233f9b2d47a3caade33940546d68ae3
JamesRobertLundberg <JamesRobertLundberg@hotmail.com>
2020-09-25 22:49:24 -0700

    Improved test files

--- Handler/PartialTest.bat
+++ Handler/PartialTest.bat
@@ -18,31 +18,30 @@ SET JOB=1185840_202003250942
 echo Copying Job %JOB% files one at a time
 SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
 timeout 5
-SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
+SlowCopy "test\%JOB% - Start\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
 timeout 5
-SlowCopy "test\%JOB%\%JOB%_mode2.csv" "%buffer%\%JOB%\%JOB%_mode2.csv"
+SlowCopy "test\%JOB% - Start\%JOB%_mode2.csv" "%buffer%\%JOB%\%JOB%_mode2.csv"
 timeout 5
-SlowCopy "test\%JOB%\Cap_Template.tab" "%buffer%\%JOB%\Cap_Template.tab"
+SlowCopy "test\%JOB% - Start\Cap_Template.tab" "%buffer%\%JOB%\Cap_Template.tab"
 timeout 5
-SlowCopy "test\%JOB%\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
+SlowCopy "test\%JOB% - Start\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
 timeout 5
-SlowCopy "test\%JOB%\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
-
+SlowCopy "test\%JOB% - Start\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
 timeout 10
 
 SET JOB=1307106_202002181307
 echo Copying Job %JOB% files one at a time
-SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
-timeout 5
-SlowCopy "test\%JOB%\%JOB%_mode2.csv" "%buffer%\%JOB%\%JOB%_mode2.csv"
-timeout 5
-SlowCopy "test\%JOB%\Cap_Template.tab" "%buffer%\%JOB%\Cap_Template.tab"
-timeout 5
-SlowCopy "test\%JOB%\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
-timeout 5
-SlowCopy "test\%JOB%\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
-timeout 5
 SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
+timeout 5
+SlowCopy "test\%JOB% - Start\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
+timeout 5
+SlowCopy "test\%JOB% - Start\%JOB%_mode2.csv" "%buffer%\%JOB%\%JOB%_mode2.csv"
+timeout 5
+SlowCopy "test\%JOB% - Start\Cap_Template.tab" "%buffer%\%JOB%\Cap_Template.tab"
+timeout 5
+SlowCopy "test\%JOB% - Start\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
+timeout 5
+SlowCopy "test\%JOB% - Start\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
 
 timeout 10
 
@@ -50,15 +49,15 @@ SET JOB=1178350_202006030856
 echo Copying Job %JOB% files one at a time
 SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
 timeout 5
-SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
+SlowCopy "test\%JOB% - Start\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
 timeout 5
-SlowCopy "test\%JOB%\%JOB%_mode2.csv" "%buffer%\%JOB%\%JOB%_mode2.csv"
+SlowCopy "test\%JOB% - Start\%JOB%_mode2.csv" "%buffer%\%JOB%\%JOB%_mode2.csv"
 timeout 5
-SlowCopy "test\%JOB%\Cap_Template.tab" "%buffer%\%JOB%\Cap_Template.tab"
+SlowCopy "test\%JOB% - Start\Cap_Template.tab" "%buffer%\%JOB%\Cap_Template.tab"
 timeout 5
-SlowCopy "test\%JOB%\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
+SlowCopy "test\%JOB% - Start\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
 timeout 5
-SlowCopy "test\%JOB%\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
+SlowCopy "test\%JOB% - Start\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
 
 timeout 10
 
@@ -66,15 +65,15 @@ SET JOB=1202740_202006110832
 echo Copying Job %JOB% files one at a time
 SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
 timeout 5
-SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
+SlowCopy "test\%JOB% - Start\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
 timeout 5
-SlowCopy "test\%JOB%\%JOB%_mode2.csv" "%buffer%\%JOB%\%JOB%_mode2.csv"
+SlowCopy "test\%JOB% - Start\%JOB%_mode2.csv" "%buffer%\%JOB%\%JOB%_mode2.csv"
 timeout 5
-SlowCopy "test\%JOB%\Cap_Template.tab" "%buffer%\%JOB%\Cap_Template.tab"
+SlowCopy "test\%JOB% - Start\Cap_Template.tab" "%buffer%\%JOB%\Cap_Template.tab"
 timeout 5
-SlowCopy "test\%JOB%\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
+SlowCopy "test\%JOB% - Start\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
 timeout 5
-SlowCopy "test\%JOB%\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
+SlowCopy "test\%JOB% - Start\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
 
 timeout 10
 
@@ -82,15 +81,15 @@ SET JOB=1185841_202005070801
 echo Copying Job %JOB% files one at a time
 SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
 timeout 5
-SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
+SlowCopy "test\%JOB% - Start\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
 timeout 5
-SlowCopy "test\%JOB%\%JOB%_mode2.csv" "%buffer%\%JOB%\%JOB%_mode2.csv"
+SlowCopy "test\%JOB% - Start\%JOB%_mode2.csv" "%buffer%\%JOB%\%JOB%_mode2.csv"
 timeout 5
-SlowCopy "test\%JOB%\Cap_Template.tab" "%buffer%\%JOB%\Cap_Template.tab"
+SlowCopy "test\%JOB% - Start\Cap_Template.tab" "%buffer%\%JOB%\Cap_Template.tab"
 timeout 5
-SlowCopy "test\%JOB%\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
+SlowCopy "test\%JOB% - Start\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
 timeout 5
-SlowCopy "test\%JOB%\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
+SlowCopy "test\%JOB% - Start\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
 
 timeout 10
 
@@ -98,15 +97,15 @@ SET JOB=1202741_202003101418
 echo Copying Job %JOB% files one at a time
 SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
 timeout 5
-SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
+SlowCopy "test\%JOB% - Start\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
 timeout 5
-SlowCopy "test\%JOB%\%JOB%_mode2.csv" "%buffer%\%JOB%\%JOB%_mode2.csv"
+SlowCopy "test\%JOB% - Start\%JOB%_mode2.csv" "%buffer%\%JOB%\%JOB%_mode2.csv"
 timeout 5
-SlowCopy "test\%JOB%\Cap_Template.tab" "%buffer%\%JOB%\Cap_Template.tab"
+SlowCopy "test\%JOB% - Start\Cap_Template.tab" "%buffer%\%JOB%\Cap_Template.tab"
 timeout 5
-SlowCopy "test\%JOB%\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
+SlowCopy "test\%JOB% - Start\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
 timeout 5
-SlowCopy "test\%JOB%\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
+SlowCopy "test\%JOB% - Start\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
 
 timeout 10
 
@@ -114,15 +113,15 @@ SET JOB=1178352_202005050818
 echo Copying Job %JOB% files one at a time
 SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
 timeout 5
-SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
+SlowCopy "test\%JOB% - Start\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
 timeout 5
-SlowCopy "test\%JOB%\%JOB%_mode2.csv" "%buffer%\%JOB%\%JOB%_mode2.csv"
+SlowCopy "test\%JOB% - Start\%JOB%_mode2.csv" "%buffer%\%JOB%\%JOB%_mode2.csv"
 timeout 5
-SlowCopy "test\%JOB%\Cap_Template.tab" "%buffer%\%JOB%\Cap_Template.tab"
+SlowCopy "test\%JOB% - Start\Cap_Template.tab" "%buffer%\%JOB%\Cap_Template.tab"
 timeout 5
-SlowCopy "test\%JOB%\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
+SlowCopy "test\%JOB% - Start\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
 timeout 5
-SlowCopy "test\%JOB%\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
+SlowCopy "test\%JOB% - Start\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
 
 timeout 10
 
@@ -130,15 +129,15 @@ SET JOB=1278061_202006301109
 echo Copying Job %JOB% files one at a time
 SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
 timeout 5
-SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
+SlowCopy "test\%JOB% - Start\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
 timeout 5
-SlowCopy "test\%JOB%\%JOB%_mode2.csv" "%buffer%\%JOB%\%JOB%_mode2.csv"
+SlowCopy "test\%JOB% - Start\%JOB%_mode2.csv" "%buffer%\%JOB%\%JOB%_mode2.csv"
 timeout 5
-SlowCopy "test\%JOB%\Cap_Template.tab" "%buffer%\%JOB%\Cap_Template.tab"
+SlowCopy "test\%JOB% - Start\Cap_Template.tab" "%buffer%\%JOB%\Cap_Template.tab"
 timeout 5
-SlowCopy "test\%JOB%\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
+SlowCopy "test\%JOB% - Start\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
 timeout 5
-SlowCopy "test\%JOB%\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
+SlowCopy "test\%JOB% - Start\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
 
 timeout 10
 
@@ -146,14 +145,14 @@ SET JOB=1610789_201911281057
 echo Copying Job %JOB% files one at a time
 SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
 timeout 5
-SlowCopy "test\%JOB%\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
+SlowCopy "test\%JOB% - Start\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
 timeout 5
-SlowCopy "test\%JOB%\%JOB%_mode2.csv" "%buffer%\%JOB%\%JOB%_mode2.csv"
+SlowCopy "test\%JOB% - Start\%JOB%_mode2.csv" "%buffer%\%JOB%\%JOB%_mode2.csv"
 timeout 5
-SlowCopy "test\%JOB%\Cap_Template.tab" "%buffer%\%JOB%\Cap_Template.tab"
+SlowCopy "test\%JOB% - Start\Cap_Template.tab" "%buffer%\%JOB%\Cap_Template.tab"
 timeout 5
-SlowCopy "test\%JOB%\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
+SlowCopy "test\%JOB% - Start\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
 timeout 5
-SlowCopy "test\%JOB%\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
+SlowCopy "test\%JOB% - Start\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
 
 :End
