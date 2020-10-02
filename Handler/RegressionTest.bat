dd4d1fc600d7555c9f2d538b3aed1793bf0d22b6
JamesRobertLundberg <JamesRobertLundberg@hotmail.com>
2020-10-01 19:46:25 -0700

    Bumped timeout

--- Handler/RegressionTest.bat
+++ Handler/RegressionTest.bat
@@ -1,125 +1,125 @@
 call FullTest
-timeout 320
+timeout 340
 call MixedTest
 timeout 140
 
 call FullTest
-timeout 320
+timeout 340
 call MixedTest
 timeout 140
 
 call FullTest
-timeout 320
+timeout 340
 call MixedTest
 timeout 140
 
 call FullTest
-timeout 320
+timeout 340
 call MixedTest
 timeout 140
 
 call FullTest
-timeout 320
+timeout 340
 call MixedTest
 timeout 140
 
 call FullTest
-timeout 320
+timeout 340
 call MixedTest
 timeout 140
 
 call FullTest
-timeout 320
+timeout 340
 call MixedTest
 timeout 140
 
 call FullTest
-timeout 320
+timeout 340
 call MixedTest
 timeout 140
 
 call FullTest
-timeout 320
+timeout 340
 call MixedTest
 timeout 140
 
 call FullTest
-timeout 320
+timeout 340
 call MixedTest
 timeout 140
 
 call FullTest
-timeout 320
+timeout 340
 call MixedTest
 timeout 140
 
 call FullTest
-timeout 320
+timeout 340
 call MixedTest
 timeout 140
 
 call FullTest
-timeout 320
+timeout 340
 call MixedTest
 timeout 140
 
 call FullTest
-timeout 320
+timeout 340
 call MixedTest
 timeout 140
 
 call FullTest
-timeout 320
+timeout 340
 call MixedTest
 timeout 140
 
 call FullTest
-timeout 320
+timeout 340
 call MixedTest
 timeout 140
 
 call FullTest
-timeout 320
+timeout 340
 call MixedTest
 timeout 140
 
 call FullTest
-timeout 320
+timeout 340
 call MixedTest
 timeout 140
 
 call FullTest
-timeout 320
+timeout 340
 call MixedTest
 timeout 140
 
 call FullTest
-timeout 320
+timeout 340
 call MixedTest
 timeout 140
 
 call FullTest
-timeout 320
+timeout 340
 call MixedTest
 timeout 140
 
 call FullTest
-timeout 320
+timeout 340
 call MixedTest
 timeout 140
 
 call FullTest
-timeout 320
+timeout 340
 call MixedTest
 timeout 140
 
 call FullTest
-timeout 320
+timeout 340
 call MixedTest
 timeout 140
 
 call FullTest
-timeout 320
+timeout 340
 call MixedTest
 timeout 140
 
