4f20323d1ce2df0b4c8dca72c711d09f7404c64a
JamesRobertLundberg <JamesRobertLundberg@hotmail.com>
2020-10-09 04:53:33 -0700

    Updated test to make it harder.

--- Handler/PartialTest.bat
+++ Handler/PartialTest.bat
@@ -14,8 +14,6 @@ set DELAY=10
 
 REM Copy directories into the Input Buffer
 
-SET JOB=1185840_202003250942
-echo Copying Job %JOB% files one at a time
 SlowCopy "test\%JOB%\75300037D00.xml" "%buffer%\%JOB%\75300037D00.xml"
 timeout 5
 SlowCopy "test\%JOB% - Start\%JOB%_mode1.csv" "%buffer%\%JOB%\%JOB%_mode1.csv"
@@ -23,10 +21,6 @@ timeout 5
 SlowCopy "test\%JOB% - Start\%JOB%_mode2.csv" "%buffer%\%JOB%\%JOB%_mode2.csv"
 timeout 5
 SlowCopy "test\%JOB% - Start\Cap_Template.tab" "%buffer%\%JOB%\Cap_Template.tab"
-timeout 5
-SlowCopy "test\%JOB% - Start\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
-timeout 5
-SlowCopy "test\%JOB% - Start\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
 timeout 10
 
 SET JOB=1307106_202002181307
@@ -38,10 +32,6 @@ timeout 5
 SlowCopy "test\%JOB% - Start\%JOB%_mode2.csv" "%buffer%\%JOB%\%JOB%_mode2.csv"
 timeout 5
 SlowCopy "test\%JOB% - Start\Cap_Template.tab" "%buffer%\%JOB%\Cap_Template.tab"
-timeout 5
-SlowCopy "test\%JOB% - Start\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
-timeout 5
-SlowCopy "test\%JOB% - Start\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
 
 timeout 10
 
@@ -54,6 +44,23 @@ timeout 5
 SlowCopy "test\%JOB% - Start\%JOB%_mode2.csv" "%buffer%\%JOB%\%JOB%_mode2.csv"
 timeout 5
 SlowCopy "test\%JOB% - Start\Cap_Template.tab" "%buffer%\%JOB%\Cap_Template.tab"
+timeout 5
+SlowCopy "test\%JOB% - Start\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
+timeout 5
+SlowCopy "test\%JOB% - Start\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
+
+
+SET JOB=1307106_202002181307
+echo Copying rest of Job %JOB% files one at a time
+timeout 5
+SlowCopy "test\%JOB% - Start\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
+timeout 5
+SlowCopy "test\%JOB% - Start\%JOB%_mode0.csv" "%buffer%\%JOB%\%JOB%_mode0.csv"
+
+timeout 10
+
+SET JOB=1185840_202003250942
+echo Copying rest of Job %JOB% files one at a time
 timeout 5
 SlowCopy "test\%JOB% - Start\Tune_Template.tab" "%buffer%\%JOB%\Tune_Template.tab"
 timeout 5
